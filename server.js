9d0b21761fc4664a6d7c70727264c7fa3a2a716c
YASHOTKARSH <businessyashotkarsh@gmail.com>
2026-03-21 17:41:30 +0530

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
+import cors from 'cors';
 import app from './src/app.js';
 import connectDB from './src/config/db.js';
 
 dotenv.config();
+app.use(cors());
 
 connectDB();
 
--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import cors from 'cors';
+
 import teamRoutes from './routes/teamRoutes.js';
 import adminRoutes from "./routes/adminRoutes.js";
 import blogRoutes from "./routes/blogRoutes.js"
 import contactRoutes from "./routes/contactRoutes.js"
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
 app.use("/api",teamRoutes);
